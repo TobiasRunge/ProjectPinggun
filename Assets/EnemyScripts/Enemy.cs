3d0e5f94ddda6430932280d6fa61c22f9764e1f8
Tobias <tobiasrunge26@gmail.com>
2024-01-15 06:09:10 +0100

    Movement + Shooting
    Added moving left, right and jumping.
    Added croucing.
    Added Shooting Scripts.
    Added Basic Enemy Script.
    Added Temporary Tilemap + Background.
    Added Cinemachine Camera.
    Added Camera following of player.
    Added A BUNCH OF ASSETS APARENTLY (1063)
    
    Missing actualy graphics + developed level design + Enemy sprite + Animation + A little bit of photoshopping with main player sprite missing gun / rifle.

---
+++ Assets/EnemyScripts/Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public int health = 100;
+
+    public GameObject deathEffect;
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
